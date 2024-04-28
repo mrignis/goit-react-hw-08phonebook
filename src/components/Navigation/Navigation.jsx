77e308e7a3bc6da3ef8fe18c2bef90fc2484bf6b
mrignis <chelbarah9678@gmail.com>
2024-04-28 04:16:44 -0400

    up

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,52 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-import { selectIsLoggedIn } from "../../redux/auth/selectors"; // Імпортуємо відповідний селектор
-import {
-  selectToken,
-  selectIsLoading,
-  selectIsError,
-} from "../../redux/auth/selectors";
-
-import { selectUserData } from "../../redux/auth/selectors"; // Імпортуємо відповідний селектор
-import routes from "../../Routes/Routes";
-import styles from "./Navigation.module.css";
-
-const Navigation = () => {
-  const isAuthenticated = useSelector(selectIsLoggedIn); // Використовуємо відповідний селектор
-
-  const userData = useSelector(selectUserData);
-  const token = useSelector(selectToken);
-  const isLoading = useSelector(selectIsLoading);
-  const isError = useSelector(selectIsError);
+export const Navigation = () => {
+  const onLogout = () => {
+    console.log(true);
+  };
 
   return (
     <nav>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <NavLink
-            exact="true"
-            to={routes.home}
-            className={styles.link}
-            activeclassname={styles["link--active"]}
-          >
-            Home
-          </NavLink>
-        </li>
+      <NavLink to="/">Home</NavLink>
 
-        {isAuthenticated && (
-          <li>
-            <NavLink
-              to={routes.contacts}
-              className={styles.link}
-              activeClassName={styles["link--active"]}
-            >
-              Contacts
-            </NavLink>
-          </li>
-        )}
-      </ul>
+      <NavLink to="/contacts">Contacts</NavLink>
+      <NavLink to="/search">Search</NavLink>
+      <div>
+        <span>Hi</span>
+        <button onClick={onLogout} type="button">
+          Logout
+        </button>
+      </div>
+
+      <NavLink to="/register">Register</NavLink>
+      <NavLink to="/login">Login</NavLink>
     </nav>
   );
 };
